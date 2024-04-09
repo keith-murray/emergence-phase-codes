4a2e1b233b372221528cc6b921cd0a9a2a13737d
ktmurray1999 <ktmurray@mit.edu>
2024-04-09 18:20:44 -0400

    Added file from hyperparameter v3

---
+++ scripts/hyperparameter_search_3/hyperparameter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Initialize and load modules
+source /etc/profile
+module load anaconda/2023a-tensorflow
+
+# Output task info
+echo "My task ID: $LLSUB_RANK"
+echo "Number of Tasks: $LLSUB_SIZE"
+
+# Execute the Python script for this task
+python /home/gridsan/kmurray/mod-rnn/modularRNN/pipeline.py $LLSUB_RANK /home/gridsan/kmurray/mod-rnn/data/hyperparameter_3
