2048429977ab7f4932a731d4feeba5f630870876
Keith T. Murray <murraykeith64@gmail.com>
2025-05-26 10:41:54 +0200

    [feat] initialize ctrnn with activation function

--- emergence_phase_codes/model.py
+++ emergence_phase_codes/model.py
@@ -2,6 +2,7 @@
 
 import jax.numpy as jnp
 from flax import linen as nn
+from flax.linen.activation import tanh, sigmoid
 
 from ctrnn_jax.model import CTRNNCell
 
@@ -16,6 +17,30 @@ def initialize_ctrnn(
             output_features=output_features,
             alpha=jnp.float32(alpha),
             noise_const=jnp.float32(noise_const),
+        ),
+        split_rngs={"params": False, "noise_stream": True},
+    )
+
+
+def initialize_ctrnn_with_activation(
+    hidden_features=100,
+    output_features=2,
+    alpha=0.1,
+    noise_const=0.05,
+    activation_fn="tanh",
+):
+    """Initialization function with activation function."""
+    activation_dict = {
+        "tanh": tanh,
+        "sigmoid": sigmoid,
+    }
+    return nn.RNN(
+        CTRNNCell(
+            hidden_features=hidden_features,
+            output_features=output_features,
+            alpha=jnp.float32(alpha),
+            noise_const=jnp.float32(noise_const),
+            activation_fn=activation_dict[activation_fn],
         ),
         split_rngs={"params": False, "noise_stream": True},
     )
